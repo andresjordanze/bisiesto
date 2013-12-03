8648c52986bd0929628f6780d85fe58d78e79896
andresjordanze <andresjordanze@gmail.com>
2013-12-03 08:51:06 -0400

    Respuesta4.1

--- src/test/java/Bisiesto/BisiestoTest.java
+++ src/test/java/Bisiesto/BisiestoTest.java
@@ -22,14 +22,4 @@ public class BisiestoTest {
         assertEquals(true,bs.esBisiesto(4));//es bisiesto
         assertEquals(false,bs.esBisiesto(100));//no es bisiesto
     }
-
-/*
-    public void t() {
-        YearUtilities instance = new YearUtilities();
-        assertTrue(instance.isLeap(4));//es bisiesto
-        assertFalse(instance.isLeap(100));//no es bisiesto
-        assertTrue(instance.isLeap(400));//es bisiesto
-        assertFalse(instance.isLeap(2011));//no es bisiesto
-        assertTrue(instance.isLeap(2012));//es bisiesto
-    }*/
 }
